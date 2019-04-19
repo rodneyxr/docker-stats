9bda0ba12b36e0e5ffdfe6c7672588d644ac4d16
Rodney Rodriguez <rodneyxr@gmail.com>
2019-04-19 14:33:26 -0500

    Add simple test function to test custom docker files

---
+++ docker/parser_test.go
@@ -0,0 +1,34 @@
+package docker
+
+import (
+	"log"
+	"testing"
+)
+
+var sampleDockerfile = `FROM golang:1.10.0
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+                openssh-client \
+                rsync \
+                fuse \
+                sshfs \
+        && rm -rf /var/lib/apt/lists/*
+RUN go get  golang.org/x/lint/golint \
+            github.com/mattn/goveralls \
+            golang.org/x/tools/cover
+ENV USER root
+WORKDIR /go/src/github.com/docker/machine
+COPY . ./
+RUN mkdir bin
+`
+
+func TestParser(t *testing.T) {
+	// Parse the Dockerfile
+	runCommandList, err := ExtractRunCommandsFromDockerfile(sampleDockerfile)
+	if err != nil {
+		log.Print(err)
+	}
+	for _, cmd := range runCommandList {
+		AnalyzeRunCommand(cmd)
+	}
+}
